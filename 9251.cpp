38f0582113e2977fcc18a847a24d54596bc59aad
NDJ's HOME PC <ehdwls6856@gmail.com>
2018-02-01 01:17:17 +0900

    9251.LCS 문제 Source 일부 수정

--- 9251.cpp
+++ 9251.cpp
@@ -9,10 +9,10 @@ int getMax(string s1, string s2) {
 	int max = 0;
 	for (int i = 1; i <= s2.length(); i++) {
 		for (int j = 1; j <= s1.length(); j++) {
-			if (s1[j - 1] == s2[i - 1]) {
+			if (s1[j - 1] == s2[i - 1]) { // 두 문자열의 문자가 서로 일치한 경우
 				dp[i][j] = dp[i - 1][j - 1] + 1;
 			}
-			else {
+			else { // 문자가 서로 다른 경우
 				dp[i][j] = dp[i - 1][j] > dp[i][j - 1] ? dp[i - 1][j] : dp[i][j - 1];
 			}
 			max = dp[i][j] > max ? dp[i][j] : max;
@@ -25,12 +25,7 @@ int main() {
 	int result = 0;
 	string s1, s2;
 	cin >> s1 >> s2;	
-	
-	if (s1.length() > s2.length())
-		result = getMax(s1, s2);
-	else
-		result = getMax(s2, s1);
-
+	result = getMax(s1, s2);
 	cout << result << "\n";
 	return 0;
 }
