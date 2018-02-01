4541d248e6bfe24ee4f9c6984d66a5ae7e8bb929
NDJ's HOME PC <ehdwls6856@gmail.com>
2018-02-02 02:11:54 +0900

    3745. 오름세 (LCS 문제) 숫자 입력받는 부분 수정 외에는 LCS 문제와 별 차이 없음.

---
+++ 3745.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	int n;
+
+	// 입력 경우에만 예외 처리하게 되면 LCS 문제와 사실상 소스는 동일
+	while ((cin >> n) && !cin.eof()) {
+		std::vector<int> num_vec(n);
+		std::vector<int> dp;
+		for (int i = 0; i < n; i++) {
+			cin >> num_vec[i];
+		}
+		dp.push_back(num_vec[0]);
+		for (int i = 0; i < n; i++) {
+			if (dp.back() < num_vec[i]) {
+				dp.push_back(num_vec[i]);
+			}
+			else {
+				auto it = std::lower_bound(dp.begin(), dp.end(), num_vec[i]);
+				*it = num_vec[i];
+			}
+		}
+		cout << dp.size() << "\n";
+	}
+	return 0;
+}
